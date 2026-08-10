a69b0d5df0741d99fbbf3487f5475ff6aa75bae2
prajwalbasnet383-hue <prajwalbasnet383@gmail.com>
2026-08-10 17:33:48 +0545

    This is the html where i used several tags to make table, form and own media and images.

---
+++ num.py
@@ -0,0 +1,9 @@
+# n=int(input("Enter any number:"))
+# a=int(input("Enter any number:"))
+# Product=n*a
+# print("The product of n*a =:",Product)
+
+
+# l=int(input("Enter any number:"))
+# A= l**2
+# print("Area of Square:",A)
